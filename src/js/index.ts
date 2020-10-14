d5d6888d92670cd18060209eeef2c6d3ea1dcbd1
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-10-14 15:34:57 +0200

    HTML updated design, TS added, css added

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,39 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let SubmitButton : HTMLButtonElement = <HTMLButtonElement> document.getElementById("submitButton");
+SubmitButton.addEventListener("click", (e:Event) => onClick()) ;
+let modifiedText:string;
+let index:number =1;
+
+function onClick() : void
+{
+    let inputTextBox : HTMLInputElement = <HTMLInputElement> document.getElementById("inputTextBox");
+    let inputText :string = inputTextBox.value;
+
+    let option : HTMLSelectElement = <HTMLSelectElement> document.getElementById("selector");
+    
+    if(option.value == "toUpper"){modifiedText= inputText.toUpperCase()}
+    if(option.value == "toLower"){modifiedText = inputText.toLowerCase()}
+
+    inputTextBox.value = ""
+    console.log("Insterted text: " + inputText);
+    console.log("Modified text: " + modifiedText);
+    AddToList(inputText,modifiedText)
+
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+function AddToList(baseText:string, modifiedText:string)
+{
+    let table : HTMLTableElement = <HTMLTableElement>document.getElementById("resultList")
+    var row:HTMLTableRowElement = table.insertRow(-1)
+    if(index %2 ==0) {row.className="resultRowRed"}
+    else{row.className="resultRowGreen"}
+    var newCell1 = row.insertCell(0)
+    var newCell2 = row.insertCell(1)
+    var newCell3 = row.insertCell(2)
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+    
+    newCell1.appendChild(document.createTextNode(index+"."));
+    newCell2.appendChild(document.createTextNode(baseText));
+    newCell3.appendChild(document.createTextNode(modifiedText));
+
+    index++
+}
